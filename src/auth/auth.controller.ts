23f564a79ce40c1ad9bddcfd24a461c019357edf
Vali <valireyimbergenov79@gmail.com>
2024-12-15 14:14:57 +0500

    auth update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -44,7 +44,7 @@ export class AuthController {
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
-    return res.status(200).json({ accessToken });
+    return res.status(200).json({ accessToken, refreshToken });
   }
 
   @UseGuards(AuthGuard)
