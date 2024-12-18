492f7de16a0afb70ee3fdc8b0e87891e3599585a
Vali <valireyimbergenov79@gmail.com>
2024-12-18 14:30:15 +0500

    update

--- src/students/dto/update-student.dto.ts
+++ src/students/dto/update-student.dto.ts
@@ -1,9 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateStudentDto } from './create-student.dto';
-import { IsOptional, IsString, IsEmail, MinLength, MaxLength, IsIn, isPhoneNumber, max, min, isString } from 'class-validator';
-import { isNamedType } from 'graphql';
-
-
+import { IsOptional, IsString, } from 'class-validator';
 export class UpdateStudentDto {
   @IsString()
   firstName?: string;
