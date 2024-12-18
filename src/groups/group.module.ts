4e1a8957f646c5ba04c4a1f70b883a47a1f0fea4
Vali <valireyimbergenov79@gmail.com>
2024-12-18 15:21:21 +0500

    update

--- src/groups/group.module.ts
+++ src/groups/group.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupsService } from './group.service';
 import { GroupsController } from './group.controller';
 import { Group } from './entities/group.entity';
+import { Student } from 'src/students/entities/user.entity';
+import { Course } from 'src/courses/entities/course.entity';
+import { Teacher } from 'src/teacher/entities/teacher.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group])],
+  imports: [TypeOrmModule.forFeature([Group, Student, Course, Teacher])],
   controllers: [GroupsController],
   providers: [GroupsService],
   exports: [GroupsService],
