fbcb06ad71e997ffb44f2af7da5e2634dcce391d
Vali <valireyimbergenov79@gmail.com>
2024-12-19 21:56:27 +0500

    update

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TeachersController } from './teacher.controller';
 import { TeachersService } from './teacher.service';
 import { Teacher } from './entities/teacher.entity';
+import { Group } from 'src/groups/entities/group.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Teacher])],
+  imports: [TypeOrmModule.forFeature([Teacher, Group])],
   controllers: [TeachersController],
   providers: [TeachersService],
 })
