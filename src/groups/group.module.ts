bc7aff51eadcec00be1e0d0a6a998bc748761825
Vali <valireyimbergenov79@gmail.com>
2024-12-20 17:49:09 +0500

    update

--- src/groups/group.module.ts
+++ src/groups/group.module.ts
@@ -4,10 +4,12 @@ import { Group } from './entities/group.entity';
 import { Teacher } from '../teacher/entities/teacher.entity';
 import { Course } from 'src/courses/entities/course.entity';
 import { Student } from 'src/students/entities/user.entity';
+import { GroupsController } from './group.controller';
+import { GroupsService } from './group.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Group, Teacher, Course, Student])],
-  controllers: [],
-  providers: [],
+  controllers: [GroupsController],
+  providers: [GroupsService],
 })
 export class GroupsModule {}
--- src/students/student.module.ts
+++ src/students/student.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentsController } from './student.controller';
 import { StudentsService } from './student.service';
 import { Student } from "./entities/user.entity";
+import { Group } from 'src/groups/entities/group.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student])],
+  imports: [TypeOrmModule.forFeature([Student, Group])],
   controllers: [StudentsController],
   providers: [StudentsService],
 })
