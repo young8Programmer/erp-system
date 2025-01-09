9e1872495e38df10c150c3b450081c0e9772166a
Vali <valireyimbergenov79@gmail.com>
2025-01-09 21:42:13 +0500

    create

--- src/submissions/dto/create-submission.dto.ts
+++ src/submissions/dto/create-submission.dto.ts
@@ -3,8 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateSubmissionDto {
   @IsNotEmpty()
   @IsString()
-  content: string; // Topshiriq javobi (fayl URL yoki matn)
-
-  @IsNotEmpty()
-  assignmentId: number; // Qaysi topshiriqqa tegishli
+  content: string;
 }
