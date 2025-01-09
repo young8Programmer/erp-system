a977ed73184db8e7ec3caf7c5b66445b095fe151
Vali <valireyimbergenov79@gmail.com>
2025-01-09 23:00:44 +0500

    create

--- src/submissions/submissions.controller.ts
+++ src/submissions/submissions.controller.ts
@@ -8,55 +8,65 @@ import {
   Req,
   ForbiddenException,
   NotFoundException,
+  UseGuards,
 } from '@nestjs/common';
 import { SubmissionService } from './submissions.service';
 import { CreateSubmissionDto } from './dto/create-submission.dto';
 import { GradeSubmissionDto } from './dto/GradeSubmissionDto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RolesStudentGuard } from 'src/auth/rolesStudentGuard';
+import { Roles } from 'src/auth/roles.guard';
+import { RolesTeacherGuard } from 'src/auth/rolesTeacherGuard';
 
 @Controller('submissions')
 export class SubmissionController {
   constructor(private readonly submissionsService: SubmissionService) {}
 
-  // Student tomonidan topshiriqqa javob yuborish
-@Post(':assignmentId/submit')
-async submitAnswer(
-  @Req() req,
-  @Param('assignmentId') assignmentId: number,
-  @Body() createSubmissionDto: CreateSubmissionDto,
-) {
-  if (!req.user || !req.user.id) {
-    throw new ForbiddenException('User not authenticated');
+
+  @UseGuards(AuthGuard, RolesStudentGuard)
+  @Roles("student")
+  @Post(':assignmentId/submit')
+  async submitAnswer(
+    @Req() req,
+    @Param('assignmentId') assignmentId: number,
+    @Body() createSubmissionDto: CreateSubmissionDto,
+  ) {
+    if (!req.user || !req.user.id) {
+      throw new ForbiddenException('User not authenticated');
+    }
+
+    const userId = req.user.id;
+    return this.submissionsService.submitAnswer(
+      userId,
+      assignmentId,
+      createSubmissionDto.content,
+    );
   }
 
-  const userId = req.user.id;
-  return this.submissionsService.submitAnswer(
-    userId,
-    assignmentId,
-    createSubmissionDto.content,
-  );
-}
+  // O'qituvchi tomonidan submission baholash
+  @UseGuards(AuthGuard, RolesTeacherGuard)
+  @Roles("teacher")
+  @Patch(':submissionId/grade')
+  async gradeSubmission(
+    @Req() req,
+    @Param('submissionId') submissionId: number,
+    @Body() gradeSubmissionDto: GradeSubmissionDto,
+  ) {
+    if (!req.user || !req.user.id) {
+      throw new ForbiddenException('User not authenticated');
+    }
 
-// O'qituvchi tomonidan submission baholash
-@Patch(':submissionId/grade')
-async gradeSubmission(
-  @Req() req,
-  @Param('submissionId') submissionId: number,
-  @Body() gradeSubmissionDto: GradeSubmissionDto,
-) {
-  if (!req.user || !req.user.id) {
-    throw new ForbiddenException('User not authenticated');
+    const userId = req.user.id;
+    return this.submissionsService.gradeSubmission(
+      userId,
+      submissionId,
+      gradeSubmissionDto.grade,
+    );
   }
-
-  const userId = req.user.id;
-  return this.submissionsService.gradeSubmission(
-    userId,
-    submissionId,
-    gradeSubmissionDto.grade,
-  );
-}
 
 
   // Talabalarning kunlik baholarini ko'rish
+  @UseGuards(AuthGuard)
   @Get('daily-grades')
   async getDailyGrades(@Req() req) {
     const userId = req.user.id; // O'qituvchi yoki adminni tekshirish uchun
@@ -64,6 +74,7 @@ async gradeSubmission(
   }
 
   // Talabalarning jami ballarini kamayish tartibida ko'rish
+  @UseGuards(AuthGuard)
   @Get('total-scores')
   async getTotalScores(@Req() req) {
     const userId = req.user.id; // O'qituvchi yoki adminni tekshirish uchun
