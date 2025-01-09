caa8dbe57d6b3afe1976619e2a3423ad4accacee
Vali <valireyimbergenov79@gmail.com>
2025-01-10 02:17:51 +0500

    create

--- src/submissions/dto/GradeSubmissionDto.ts
+++ src/submissions/dto/GradeSubmissionDto.ts
@@ -1,9 +1,8 @@
 import { IsNotEmpty, IsNumber, Min, Max } from 'class-validator';
-
 export class GradeSubmissionDto {
   @IsNotEmpty()
   @IsNumber()
   @Min(0)
-  @Max(100) // Maksimal baho 100 deb belgiladik, kerak bo'lsa o'zgartirish mumkin
-  grade: number; // Baxo qiymati
+  @Max(100)
+  grade: number
 }
